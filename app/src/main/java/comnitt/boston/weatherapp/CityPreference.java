5a4c629f444b704d1905bfca6590dd00ff041973
Kumar Paritosh <kprockzz22@gmail.com>
2017-07-14 19:26:28 +0530

    Initial commit

---
+++ app/src/main/java/comnitt/boston/weatherapp/CityPreference.java
@@ -0,0 +1,28 @@
+package comnitt.boston.weatherapp;
+
+/**
+ * Created by HP on 14-Jul-17.
+ */
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+
+public class CityPreference {
+
+    SharedPreferences prefs;
+
+    public CityPreference(Activity activity){
+        prefs = activity.getPreferences(Activity.MODE_PRIVATE);
+    }
+
+    // If the user has not chosen a city yet, return
+    // Sydney as the default city
+    String getCity(){
+        return prefs.getString("city", "Chennai, IN");
+    }
+
+    void setCity(String city){
+        prefs.edit().putString("city", city).commit();
+    }
+
+}
---
+++ app/src/main/java/comnitt/boston/weatherapp/MainActivity.java
@@ -0,0 +1,60 @@
+package comnitt.boston.weatherapp;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+//import android.app.Fragment;
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.container, new WeatherFragment()).commit();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.change_city){
+            showInputDialog();
+        }
+        return false;
+    }
+
+    public void showInputDialog()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Change city");
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+        builder.setPositiveButton("Go", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                changeCity(input.getText().toString());
+            }
+        });
+        builder.show();
+    }
+
+
+
+    public void changeCity(String city){
+        WeatherFragment wf = (WeatherFragment)getSupportFragmentManager().findFragmentById(R.id.container);
+        wf.changeCity(city);
+        new CityPreference(this).setCity(city);
+    }
+
+}
